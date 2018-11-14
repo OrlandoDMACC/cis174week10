352713dc4976cf7f10d9d8524830868458caffb5
DMACC\olwinters <olwinters@dmacc.edu>
2018-11-14 15:02:51 -0600

    11/14

---
+++ CIS174Winters/Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CIS174Winters.Models
+{
+    public class LoginViewModel
+    {
+      public  String UserName { get; set; }
+      public String Password { get; set; }
+
+
+    }
+}
